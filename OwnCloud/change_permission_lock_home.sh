4ab9b744836ea2acea96ca14b9aacded04c3e602
mdmiko <domenico.milano80@gmail.com>
2016-03-04 10:34:02 +0100

    Create change_permission_lock_home.sh

---
+++ OwnCloud/change_permission_lock_home.sh
@@ -0,0 +1,4 @@
+chown -R root:root /qnap/data/
+chown www-data:www-data /qnap/data/index.html /qnap/data/mount.json /qnap/data/$
+chown -R www-data:www-data /qnap/data/admin
+chown www-data:www-data /qnap/data
